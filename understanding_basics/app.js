80fd0ead64e6ac4e2d5780890fe0eb639b58a9dd
utsavnavadiya11 <utsavnavadiya1005@gmail.com>
2023-12-23 17:42:14 +0530

    initial commit

--- understanding_basics/app.js
+++ understanding_basics/app.js
@@ -1,9 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('Request served!!');
-    // to end the server use...
-    // process.exit()
+    // console.log('Request served!!');
+    // // to end the server use...
+    // // process.exit()
+    // res.setHeader('Content-Type','text/html');
+    // res.write('<h1>This is Home Page</h1>');
+    // res.end();
+    const url = req.url;
+    if(url === '/') {
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<form action = "/messege" method = "POST"><input type = "text"/><Button type = "submit" title = "submit"/></form>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    } else if(url === '/messege') {
+        fs.writeFileSync('dummy.txt', 'Form is Submitted')
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<h1>Response Recorded</h1>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 server.listen(5000);
